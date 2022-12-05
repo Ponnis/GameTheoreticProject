635816481414008e3c75a93d3714f0a8ba5b2af4
Aron Börjesson Lindgren <aron.borjesson.lindgren@gmail.com>
2022-12-05 13:44:05 +0100

    Don't remember, but I want to pull the latest version

--- models.py
+++ models.py
@@ -60,7 +60,7 @@ class PokemonGame():
 
 # Calculate the utility matrix for two given teams
 def calculateUtilities(team1, team2):
-    utilities = np.zeros((6, 6))
+    utilities = np.random.rand(6, 6)
 
     return utilities
 
@@ -70,12 +70,36 @@ def calculateNash(pokemon_game):
     utilities = pokemon_game.utility_matrix
     nash_game = nashpy.Game(utilities)
     nash_EQs = nash_game.support_enumeration()
-
     return list(nash_EQs)
 
 # Perform a battle with selections based on the nash_EQ and return the resulting score for each player
 # Output: [scoreP1, scoreP2]
-def battle(pokemon_game):
+def battle(pokemon_game,num_rounds):
+    p1_pick = -1
+    p2_pick = -1
     scores = [0,0]
+    num_options = len(pokemon_game.nash_eqs[0][0])
+    for n in range(num_rounds):
+        rP1 = np.random.rand()
+        rP2 = np.random.rand()
+        for k in range(num_options):
+            mixed_k1 = pokemon_game.nash_eqs[0][0][k]
+            mixed_k2 = pokemon_game.nash_eqs[0][1][k]
+            if rP1 < mixed_k1:
+                p1_pick = k
+            else:
+                rP1 -= mixed_k1
+            if rP2 < mixed_k2:
+                p2_pick = k
+            else:
+                rP2 -= mixed_k2
+
+        print(scores[0])
+        scores[0] += pokemon_game.utility_matrix[p1_pick][p2_pick][0]
+        scores[1] += pokemon_game.utility_matrix[p1_pick][p2_pick][1]
+    scores[0] = scores[0]/num_rounds
+    scores[1] = scores[1]/num_rounds
+
     return scores
+
 
