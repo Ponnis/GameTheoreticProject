229d5f3a6893a4bbc302e4b967b116e6b72770c3
Ponnis <nilsmartin.robeling@gmail.com>
2022-12-13 00:04:31 +0100

    Added support for combinations. Made the large lookup dictionary for Arons code as well as supportive methods and example.
    Signed-off-by: Ponnis <nilsmartin.robeling@gmail.com>

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import timeit
+
 import models
 import models as m
 import visualization
@@ -5,6 +7,10 @@ import visualization
 example_game = m.PokemonGame(team1=[m.Types.GRASS, m.Types.WATER, m.Types.DARK],
                              team2=[m.Types.GRASS, m.Types.WATER, m.Types.BUG])
 
-print(example_game.utility_matrix)
-print(m.splitUtilities(example_game.utility_matrix))
-models.staticWinner(2)
+b_dict = m.getLookupDict(3)
+test_team1 = [m.Types.NORMAL, m.Types.NORMAL, m.Types.NORMAL]
+test_team2 = [m.Types.WATER, m.Types.GRASS, m.Types.FIGHT]
+start = timeit.timeit()
+print(m.lookupValue(test_team1, test_team2, b_dict))
+end = timeit.timeit()
+print("Elapsed time: " + str(end - start))
